ee221ab7948d160bf179c323484ab837f00b09f7
Gustavo <gharduim@gmail.com>
2021-11-15 19:06:39 +1030

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Team Profile Generator
+
+## Description
+
+This application will allow the user to generate a site for their development team. The site will include each member's contact information as well as employee IDs and other pertinent professional information based on their role.
+
+## User Story
+
+```md
+AS A manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to their emails and GitHub profiles
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for my team members and their information
+THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+WHEN I click on an email address in the HTML
+THEN my default email program opens and populates the TO field of the email with the address
+WHEN I click on the GitHub username
+THEN that GitHub profile opens in a new tab
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I select the engineer option
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the intern option
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I decide to finish building my team
+THEN I exit the application, and the HTML is generated
+```
+
+
+### Demonstration
+
+View a complete video demonstration of the application: [Professional Team Profile Generator Demo](https://drive.google.com/file/d/1z-17o_35bS_CMjLPCu_ju3fi8b6UIA0P/view?usp=sharing)
+
+![out-Team-screen](./assets/video/Team-demo.gif "Output Team Profile file")
+
+### Output HTML
+![out-HTML-screen](./assets/screenshots/screenshot.png "Output HTML file")
+
+## Table of Contents
+
+* [Description](#description)
+* [Technologies](#technologies)
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [Questions](#questions)
+
+## Technologies
+
+* [Node.js](https://nodejs.org/)
+* [Inquirer.js](https://www.npmjs.com/package/inquirer)
+* [Jest.js](https://jestjs.io/)
+
+## Installation
+
+To install dependencies, run the following:
+
+```
+npm init -y
+```
+```
+npm install inquirer
+```
+
+## Tests
+
+If the user wishes to test the code, install jest to the development dependencies using the following command; if there is no plan to test the code, please skip this section
+
+```
+npm install jest
+```
+After installed, the user uses the following command to run the tests
+```
+npm run test
+```
+
+## Usage
+
+After installing the dependencies, run the application with
+
+```
+node app.js
+```
+
+
+## Questions
+
+Questions about this repository? Please contact me at [gharduim@gmail.com](mailto:gharduim@gmail.com). View more of my work in GitHub at [Gustavo Harduim](https://github.com/gharduim) 
---
+++ app.js
@@ -0,0 +1,137 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+const inquirer = require('inquirer')
+const path = require('path')
+const fs = require('fs')
+
+let team = []
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, 'team.html')
+
+const render = require("./lib/htmlRenderer");
+
+// Start the application by running node.js
+function start() {
+    // Add a new manager
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the manager's name?",
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: "What is the manager's ID?",
+            name: 'managerId',
+        },
+        {
+            type: 'input',
+            message: "What is the mangager's email address?",
+            name: "managerEmail",
+        },
+        {
+            type: 'input',
+            message: "What is the manager's office number?",
+            name: 'officeNumber',
+        },
+    ]).then(response => {
+        const newManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.officeNumber);
+        team.push(newManager);
+        newMember();
+    });
+}
+
+// Build a team of engineers and interns
+function newMember() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add a new team member?',
+            choices: ['Engineer', 'Intern', 'Finish'],
+            name: 'newEmployee'
+        }
+    ]).then(response => {
+        switch (response.newEmployee) {
+            case 'Engineer':
+                newEngineer();
+                break;
+            case 'Intern':
+                newIntern();
+                break;
+            case 'Finish':
+                create();
+                break;
+        }
+    })
+};
+
+// Add an engineer to the team
+function newEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the engineer's name?",
+            name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's ID number?",
+            name: 'engineerId'
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email address?",
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's Github username?",
+            name: 'engineerGithub'
+        }
+    ]).then(response => {
+        const newEngineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+        team.push(newEngineer);
+        newMember()
+    })
+}
+
+// Add an intern to the team
+function newIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: 'internName'
+        },
+        {
+            type: 'input',
+            message: "What is the intern's ID number?",
+            name: 'internId'
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email address?",
+            name: 'internEmail'
+        },
+        {
+            type: 'input',
+            message: "Where does the intern go to school?",
+            name: 'internSchool'
+        }
+    ]).then(response => {
+        const newIntern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+        team.push(newIntern);
+        newMember()
+    })
+};
+
+// creates the team to be rendered to the team.html page
+function create() {
+    fs.writeFile(outputPath, render(team), function (err) {
+        if (err) throw err;
+    })
+};
+
+start();
